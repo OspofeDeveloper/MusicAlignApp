aae7c2fb110405bd83ff104fe41f9819df5253fc
OspofeDeveloper <ospofedeveloper@gmail.com>
2024-03-02 16:16:23 +0100

    activity_align.xml v1.0 implemented

---
+++ app/src/main/java/com/example/musicalignapp/ui/align/AlignActivity.kt
@@ -0,0 +1,12 @@
+package com.example.musicalignapp.ui.align
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.musicalignapp.R
+
+class AlignActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_align)
+    }
+}
---
+++ app/src/main/java/com/example/musicalignapp/ui/align/AlignViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.musicalignapp.ui.align
+
+class AlignViewModel {
+}
